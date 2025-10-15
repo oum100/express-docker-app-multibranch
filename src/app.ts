377a193d0e61f00ac1cc33c536c69b8ae096ff98
Teerin Leepaiboon <l.teerin@gmail.com>
2025-10-15 21:44:58 +0700

    add develop branch

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,17 @@ app.get("/api/products", (_: Request, res: Response) => {
 });
 
 // GET /api/orders
+app.get('/api/orders', (_: Request, res: Response) => {
+  const orders = [
+    { id: 1, userId: 1, productId: 2, quantity: 1 },
+    { id: 2, userId: 2, productId: 3, quantity: 2 },
+    { id: 3, userId: 1, productId: 1, quantity: 1 },
+    { id: 4, userId: 2, productId: 4, quantity: 1 }
+  ]
+  res.json(orders)
+})
+
+// GET /api/orders
 app.get("/api/orders", (_: Request, res: Response) => {
   const orders = [
     { id: 1, userId: 1, productId: 2, quantity: 1 },
